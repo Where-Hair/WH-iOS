e9e005d0bb8cf300fc750f9d75ee4cab7e9f478d
JI-doo <awt0311@gmail.com>
2021-03-25 18:51:09 +0900

    [ADD] color

--- WH-iOS/WH-iOS/Source/Extension/UIColor.swift
+++ WH-iOS/WH-iOS/Source/Extension/UIColor.swift
@@ -5,4 +5,15 @@
 //  Created by 문지수 on 2021/03/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static func rgb(red: Int, green: Int, blue: Int, alpha: CGFloat) -> UIColor {
+        return .init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: alpha)
+    }
+    
+    static var mainColor: UIColor {
+        return rgb(red: 204, green: 153, blue: 255, alpha: 1)
+    }
+    
+}
