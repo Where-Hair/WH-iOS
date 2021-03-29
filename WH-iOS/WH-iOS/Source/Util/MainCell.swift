53ed6e3a7d8dd5688232b572f3ac5e3dd16c245d
JI-doo <awt0311@gmail.com>
2021-03-29 19:52:06 +0900

    [ADD] main cell

---
+++ WH-iOS/WH-iOS/Source/Util/MainCell.swift
@@ -0,0 +1,29 @@
+//
+//  MainCell.swift
+//  WH-iOS
+//
+//  Created by 문지수 on 2021/03/29.
+//
+
+import UIKit
+
+class MainCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var shopNameLbl: UILabel!
+    @IBOutlet weak var detailBtn: UIButton!
+    @IBOutlet weak var textLbl: UILabel!
+    @IBOutlet weak var shopImage: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
