bda2afea8a8f6753ccc38d5093b1158cc18271f4
JI-doo <awt0311@gmail.com>
2021-04-08 11:14:18 +0900

    [ADD] token model

---
+++ WH-iOS/WH-iOS/Source/Model/TokenModel.swift
@@ -0,0 +1,13 @@
+//
+//  TokenModel.swift
+//  WH-iOS
+//
+//  Created by 문지수 on 2021/04/08.
+//
+
+import Foundation
+
+struct TokenModel: Codable {
+    let accessToken: String
+    let refreshToken: String
+}
