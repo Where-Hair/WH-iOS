054f8c912dce6a4e2fbf9d011875c50ba4d09cbf
JI-doo <awt0311@gmail.com>
2021-04-08 10:39:17 +0900

    [ADD] service

---
+++ WH-iOS/WH-iOS/Source/Service/Service.swift
@@ -0,0 +1,39 @@
+//
+//  Service.swift
+//  WH-iOS
+//
+//  Created by 문지수 on 2021/03/30.
+//
+
+import Foundation
+
+import RxSwift
+import Moya
+
+class Service {
+    let provider = MoyaProvider<WhereHairAPI>()
+    
+    func signIn(_ id: String, _ password: String) -> Observable<Network> {
+        return provider.rx.request(.signIn(id, password))
+            .filterSuccessfulStatusCodes()
+            .asObservable()
+            .map { _ -> Network in
+                return (.success)
+            }.catchError { [unowned self] in return .just(self.setNetworkError($0)) }
+    }
+    
+    func signUp(_ nickname: String, _ id: String, _ password: String) -> Observable<Network> {
+        return provider.rx.request(.signUp(nickname, id, password))
+            .filterSuccessfulStatusCodes()
+            .asObservable()
+            .map { _ -> Network in
+                return (.success)
+            }.catchError { [unowned self] in return .just(self.setNetworkError($0))}
+    }
+    
+    func setNetworkError(_ error: Error) -> Network {
+        guard let status = (error as? MoyaError)?.response?.statusCode else { return (.fail) }
+         print(error)
+        return (Network(rawValue: status) ?? .fail)
+     }
+}
