0bb8469204edd43e0936d8bf8f3a194490f24570
JI-doo <awt0311@gmail.com>
2021-04-14 09:49:53 +0900

    [ADD] signIn viewmodel

---
+++ WH-iOS/WH-iOS/Source/ViewModel/SignInViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  SignInViewModel.swift
+//  WH-iOS
+//
+//  Created by 문지수 on 2021/04/13.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+class SignInViewModel: ViewModelType {
+    
+    private let disposeBag = DisposeBag()
+    
+    struct Input {
+        let id: Driver<String>
+        let password: Driver<String>
+        let doneTap: Driver<String>
+    }
+    struct Output {
+        let result: Signal<String>
+        let isEnalbe: Driver<Bool>
+    }
+    
+    func transform(input: Input) -> Output {
+        let api = Service()
+        let result = PublishSubject<String>()
+        let info = Driver.combineLatest(input.id, input.password)
+        let isEnable = info.map { !$0.0.isEmpty && !$0.1.isEmpty }
+        
+        input.doneTap.asObservable().withLatestFrom(info).subscribe(onNext : { [weak self] id, password in
+            guard let self = self else { return }
+            api.signIn(id, password).subscribe(onNext: { response in
+                switch response {
+                case .success:
+                    result.onCompleted()
+                case .notFound:
+                    result.onNext("notFound")
+                case .conflict:
+                    result.onNext("conflict")
+                default:
+                    print("default")
+                }
+            }).disposed(by: self.disposeBag)
+        }).disposed(by: disposeBag)
+        
+        return Output(result: result.asSignal(onErrorJustReturn: "회원가입 실패"), isEnalbe: isEnable.asDriver())
+    }
+}
+
