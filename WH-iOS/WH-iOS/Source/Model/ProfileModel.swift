a70890493c0573d6222530b79852dccbf98cb3b5
JI-doo <awt0311@gmail.com>
2021-04-15 11:35:45 +0900

    [ADD] profile model

---
+++ WH-iOS/WH-iOS/Source/Model/ProfileModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileModel.swift
+//  WH-iOS
+//
+//  Created by 문지수 on 2021/04/15.
+//
+
+import Foundation
+
+struct ProfileModel: Codable {
+    let image: String
+    let nickname: String
+}
