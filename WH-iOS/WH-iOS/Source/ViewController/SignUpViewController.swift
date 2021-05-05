a83a029832c7fc75c917538845c298c6be7dd435
JI-doo <awt0311@gmail.com>
2021-05-05 22:02:49 +0900

    [MODIFY] signUp ui

--- WH-iOS/WH-iOS/Source/ViewController/SignUpViewController.swift
+++ WH-iOS/WH-iOS/Source/ViewController/SignUpViewController.swift
@@ -18,69 +18,22 @@ class SignUpViewController: UIViewController {
     private let viewModel = SignUpViewModel()
     
     //UI
-    private let textLogo = UIImageView().then {
-        $0.image = UIImage(named: "text_Logo")
-    }
-    private let nicknameLbl = UILabel().then {
-        $0.text = "닉네임"
-        $0.textColor = .black
-        $0.font = UIFont(name: "NanumSquareRoundR", size: 17)
-    }
-    private let idLbl = UILabel().then {
-        $0.text = "아이디"
-        $0.textColor = .black
-        $0.font = UIFont(name: "NanumSquraeRoundR", size: 17)
-    }
-    private let passwordLbl = UILabel().then {
-        $0.text = "비밀번호"
-        $0.textColor = .black
-        $0.font = UIFont(name: "NanumSquareRoundR", size: 17)
-    }
-    private let nicknameTxtField = UITextField().then {
-        $0.placeholder = "nickname"
-        $0.font = UIFont(name: "NanumSquareRoundR", size: 17)
-        $0.underLine()
-    }
-    private let idTxtField = UITextField().then {
-        $0.placeholder = "id"
-        $0.font = UIFont(name: "NanumSquareRoundR", size: 17)
-        $0.underLine()
-
-    }
-    private let passwordTxtField = UITextField().then {
-        $0.placeholder = "password"
-        $0.font = UIFont(name: "NanumSquareRoundR", size: 17)
-        $0.underLine()
-    }
-    private let signUpBtn = UIButton().then {
-        $0.clipsToBounds = true
-        $0.setTitle("회원가입", for: .normal)
-        $0.setTitleColor(.mainColor, for: .normal)
-        $0.titleLabel?.font = UIFont(name: "NanumSquareRoundR", size: 25)
-    }
-    private let signInBtn = UIButton().then {
-        $0.clipsToBounds = true
-        $0.setTitle("이미 계정이 있으신가요?", for: .normal)
-        $0.setTitleColor(.black, for: .normal)
-        $0.titleLabel?.font = UIFont(name: "NanumSquareRoundR", size: 10)
-    }
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var nicknameLbl: UILabel!
+    @IBOutlet weak var nicknameTxtField: UITextField!
+    @IBOutlet weak var idLbl: UILabel!
+    @IBOutlet weak var idTxtField: UITextField!
+    @IBOutlet weak var passwordLbl: UILabel!
+    @IBOutlet weak var passwordTxtField: UITextField!
+    @IBOutlet weak var signUpBtn: UIButton!
+    @IBOutlet weak var signInBtn: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        view.addSubview(textLogo)
-        view.addSubview(nicknameLbl)
-        view.addSubview(nicknameTxtField)
-        view.addSubview(idLbl)
-        view.addSubview(idTxtField)
-        view.addSubview(passwordLbl)
-        view.addSubview(passwordTxtField)
-        view.addSubview(signInBtn)
-        view.addSubview(signUpBtn)
         
         passwordTxtField.isSecureTextEntry = true
 
-        constantraint()
     }
     
     //bind
@@ -95,61 +48,6 @@ class SignUpViewController: UIViewController {
         output.result.emit(onNext: { _ in
             self.pushVC("signInVC")
         }).disposed(by: disposeBag)
-    }
-    
-    //Constantraint
-    func constantraint() {
-        textLogo.snp.makeConstraints { (make) in
-            make.centerX.equalTo(view)
-            make.top.equalTo(view.frame.height/5)
-            make.width.height.equalTo(220)
-        }
-        nicknameLbl.snp.makeConstraints { (make) in
-            make.top.equalTo(textLogo.snp.bottom).offset(30)
-            make.centerX.equalTo(view)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-50)
-        }
-        nicknameTxtField.snp.makeConstraints { (make) in
-            make.top.equalTo(nicknameLbl.snp.bottom).offset(20)
-            make.centerX.equalTo(view)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-30)
-        }
-        idLbl.snp.makeConstraints { (make) in
-            make.top.equalTo(nicknameTxtField.snp.bottom).offset(40)
-            make.centerX.equalTo(view)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-50)
-        }
-        idTxtField.snp.makeConstraints { (make) in
-            make.top.equalTo(idLbl.snp.bottom).offset(20)
-            make.centerX.equalTo(view)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-30)
-        }
-        passwordLbl.snp.makeConstraints{ (make) in
-            make.top.equalTo(idTxtField.snp.bottom).offset(40)
-            make.centerX.equalTo(view)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-30)
-        }
-        passwordTxtField.snp.makeConstraints { (make) in
-            make.top.equalTo(passwordLbl.snp.bottom).offset(20)
-            make.centerX.equalTo(view)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-30)
-        }
-        signUpBtn.snp.makeConstraints { (make) in
-            make.top.equalTo(passwordTxtField.snp.bottom).offset(30)
-            make.centerX.equalTo(view)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-30)
-        }
-        signInBtn.snp.makeConstraints { (make) in
-            make.top.equalTo(signUpBtn.snp.bottom).offset(10)
-            make.centerX.equalTo(view)
-        }
     }
     
 
