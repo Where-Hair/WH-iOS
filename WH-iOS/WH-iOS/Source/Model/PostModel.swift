6b5f900533c4fb205cd135223fd7ba61efda114c
JI-doo <awt0311@gmail.com>
2021-05-06 17:11:28 +0900

    [ADD] post model

---
+++ WH-iOS/WH-iOS/Source/Model/PostModel.swift
@@ -0,0 +1,16 @@
+//
+//  PostModel.swift
+//  WH-iOS
+//
+//  Created by 문지수 on 2021/05/06.
+//
+
+import Foundation
+
+struct PostModel: Codable {
+    let name: String
+    let location: String
+    let introduce: String
+    let information: String
+    let img: String
+}
