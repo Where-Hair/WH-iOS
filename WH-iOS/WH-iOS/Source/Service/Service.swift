89941291cb07a9a52a02865204473a0aa4a49fbc
JI-doo <awt0311@gmail.com>
2021-05-08 18:23:36 +0900

    [ADD] post service

--- WH-iOS/WH-iOS/Source/Service/Service.swift
+++ WH-iOS/WH-iOS/Source/Service/Service.swift
@@ -58,6 +58,15 @@ class Service {
             .catchError{ _ in return .just((nil, .fail))}
     }
     
+    func post(_ name: String, _ location: String, _ introduce: String, _ informaion: String, _ img: Data?) -> Observable<Network> {
+        return provider.rx.request(.post(name, location, introduce, informaion, img))
+            .filterSuccessfulStatusCodes()
+            .asObservable()
+            .map { _ -> Network in
+                return (.success)
+            }.catchError{ [unowned self] in return .just(self.setNetworkError($0))}
+    }
+    
     func setNetworkError(_ error: Error) -> Network {
         guard let status = (error as? MoyaError)?.response?.statusCode else { return (.fail) }
          print(error)
